832183815cfbc70cbd50f87af9e1268ce7eb01e1
Muhammad Uzair <143239631+ucdexpert@users.noreply.github.com>
2025-02-19 20:59:48 +0500

    growth.py

--- growth.py
+++ growth.py
@@ -78,7 +78,7 @@ if uploaded_file:
                 mime_type = "text/csv"
 
             elif conversion_type == "Excel":
-                df.to.to_excel(buffer, index=False)
+               df.to_excel(buffer, index=False)
                 file_name = file.name.replace(file_ext, ".xlsx")
                 mime_type = "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
             buffer.seek(0)
